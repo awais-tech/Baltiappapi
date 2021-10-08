efc0a6ca6babed681109fa51a484b1761f9c196d
awais <awaisshahbazrock@gmail.com>
2021-10-08 21:49:14 +0500

    chat added

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,13 @@ var express = require("express");
 const { User } = require("../models/user");
 var router = express.Router();
 
-/* GET users listing. */
 router.post("/", function (req, res, next) {
   try {
     let user = new User(req.body);
     await user.save();
     return res.status(200).send(user);
   } catch (e) {
-    return res.status(400).send(e);
+    return res.send(e);
   }
 });
 
