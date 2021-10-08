9edb5076072be86607cd0631f6de41e0e9e54cbb
awais <awaisshahbazrock@gmail.com>
2021-10-08 20:53:04 +0500

    chat added

---
+++ models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+var userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  Uid: String,
+  Phoneno: String,
+  role: {
+    type: String,
+    default: "user",
+  },
+});
+
+var User = mongoose.model("User", userSchema);
+
+module.exports.User = User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
+const { User } = require("../models/user");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  try {
+    let user = new User(req.body);
+    return res.status(200).send(user);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
 });
 
 module.exports = router;
