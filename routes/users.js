3a79b5e23a3799f7ba645bc94a0927334b2f7df6
awais <awaisshahbazrock@gmail.com>
2021-10-09 00:24:01 +0500

    chat added

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,9 @@ router.post("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     let users = await User.findById(req.params.id);
+    if (!users) {
+      return res.status(400).send({ message: "No user found" });
+    }
     return res.status(200).send(users);
   } catch (e) {
     return res.send(e);
